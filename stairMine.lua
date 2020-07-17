2047dd3b1f8c3fd6d2c33842e3a43feb75f5bc77
Eric Jayjock <ejayjock@gmail.com>
2020-07-17 13:35:00 -0400

    Added clear with a directional argument.

--- stairMine.lua
+++ stairMine.lua
@@ -31,6 +31,40 @@ function clearForward()
   end
 end
 
+function clear(direction)
+  cont=true
+  if direction == nil or direction == 'forward' then
+    -- Loop to interate until space in front of turtle is clear
+    while cont do
+      if turtle.detect() then
+        turtle.dig()
+      else
+        cont=false
+      end
+    end
+  elseif direction == 'up' then
+    -- Loop to interate until space in above turtle is clear
+    while cont do
+      if turtle.detectUp() then
+        turtle.digUp()
+      else
+        cont=false
+      end
+    end
+  elseif direction == 'down' then
+    -- Loop to interate until space in below turtle is clear
+    while cont do
+      if turtle.detectDown() then
+        turtle.digDown()
+      else
+        cont=false
+      end
+    end
+  else
+    error('function clear called incorrectly')
+  end
+end
+
 for i=1,5 do
   clearUp()
   turtle.up()
